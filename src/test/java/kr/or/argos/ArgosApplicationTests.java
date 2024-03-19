e4ba42338bcb2b509ffc009f58944d72c4746444
jeomxon <jeomxon@gmail.com>
2024-03-19 17:11:19 +0900

    Project Structure
    It is commit for very first project structure from Spring Initializer.

---
+++ src/test/java/kr/or/argos/ArgosApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.argos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
