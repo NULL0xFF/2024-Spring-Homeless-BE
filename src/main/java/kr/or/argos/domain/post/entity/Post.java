e65c6fa6e270617a7c7c561c6a6a280b1132d9af
jeomxon <jeomxon@gmail.com>
2024-03-31 23:42:00 +0900

    feat: post 엔티티 및 레포지토리 생성

--- src/main/java/kr/or/argos/domain/post/entity/Post.java
+++ src/main/java/kr/or/argos/domain/post/entity/Post.java
@@ -1,4 +1,48 @@
 package kr.or.argos.domain.post.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private long hits;
+
+    @Column(nullable = false)
+    private boolean titleBold;
+
+    @Builder
+    private Post(
+            final String title,
+            final String content,
+            final long hits,
+            final boolean titleBold
+    ) {
+        this.title = title;
+        this.content = content;
+        this.hits = hits;
+        this.titleBold = titleBold;
+    }
 }
