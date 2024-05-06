62ca3c691a355be07c0b8dbbe1df9f966130620d
jeomxon <jeomxon@gmail.com>
2024-05-06 17:16:21 +0900

    refactor: post - user 연관관계 매핑 추가

--- src/main/java/kr/or/argos/domain/post/entity/Post.java
+++ src/main/java/kr/or/argos/domain/post/entity/Post.java
@@ -2,9 +2,13 @@ package kr.or.argos.domain.post.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import kr.or.argos.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -21,6 +25,10 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
     @Column(length = 200, nullable = false)
     private String title;
 
@@ -32,10 +40,12 @@ public class Post {
 
     @Builder
     private Post(
+            final User user,
             final String title,
             final String content,
             final long hits
     ) {
+        this.user = user;
         this.title = title;
         this.content = content;
         this.hits = hits;
