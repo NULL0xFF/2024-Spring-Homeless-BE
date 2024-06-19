eb0e5bc4bb50a936643336c107070b74e805bd35
Myoung Ha, Ji <webmaster@null0xff.com>
2024-06-19 09:30:07 +0900

    :memo: User API 명세 설명 추가

--- src/main/java/kr/or/argos/domain/user/presentation/UserController.java
+++ src/main/java/kr/or/argos/domain/user/presentation/UserController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import jakarta.servlet.http.HttpServletRequest;
 import kr.or.argos.domain.user.dto.UserLogin;
 import kr.or.argos.domain.user.dto.UserRegistration;
@@ -27,72 +29,77 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Tag(name = "User Controller", description = "User API")
 public class UserController {
 
   private final UserService userService;
 
   @PostMapping("/register")
-  @Operation(summary = "Register", description = "Register to the system")
+  @Operation(summary = "Register a new user", description = "Register a new user to the system")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Register successful", content = {
           @Content(mediaType = "plain/text", schema = @Schema(implementation = Jwt.class))}),
       @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content)})
+  @Tags({@Tag(name = "Create")})
   public ResponseEntity<String> register(@RequestBody UserRegistration request) {
     return ResponseEntity.ok(userService.register(request).toString());
   }
 
   @PostMapping("/login")
-  @Operation(summary = "Login", description = "Login to the system")
+  @Operation(summary = "Login to the system", description = "Login to the system and get a json web token")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Login successful", content = {
           @Content(mediaType = "plain/text", schema = @Schema(implementation = Jwt.class))}),
       @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)})
+  @Tags({@Tag(name = "Read")})
   public ResponseEntity<String> login(@RequestBody UserLogin request) {
     return ResponseEntity.ok(userService.login(request).toString());
   }
 
   @PutMapping("/update")
-  @Operation(summary = "Update", description = "Update user information")
+  @Operation(summary = "Update user information", description = "Update user information in the system")
   @SecurityRequirement(name = "bearerAuth")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Update user information successful", content = {
           @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
       @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)})
-  public ResponseEntity<User> update(HttpServletRequest servlet,
-      @RequestBody UserUpdate request) {
+  @Tags({@Tag(name = "Update")})
+  public ResponseEntity<User> update(HttpServletRequest servlet, @RequestBody UserUpdate request) {
     return ResponseEntity.ok(userService.update(servlet, request));
   }
 
   @GetMapping("/me")
-  @Operation(summary = "Me", description = "Get user information")
+  @Operation(summary = "Get user information", description = "Get user information from the system")
   @SecurityRequirement(name = "bearerAuth")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Get user information successful", content = {
           @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))}),
       @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)})
+  @Tags({@Tag(name = "Read")})
   public ResponseEntity<User> me(HttpServletRequest servlet) {
     return ResponseEntity.ok(userService.me(servlet));
   }
 
   @GetMapping("/refresh")
-  @Operation(summary = "Refresh", description = "Refresh token")
+  @Operation(summary = "Refresh JWT", description = "Refresh json web token for the user")
   @SecurityRequirement(name = "bearerAuth")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "Refresh token successful", content = {
           @Content(mediaType = "plain/text", schema = @Schema(implementation = Jwt.class))}),
       @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)})
+  @Tags({@Tag(name = "Read")})
   public ResponseEntity<String> refresh(HttpServletRequest servlet) {
     return ResponseEntity.ok(userService.refresh(servlet.getRemoteUser()).toString());
   }
 
   @DeleteMapping("/{username}")
-  @Operation(summary = "Delete", description = "Delete user information")
+  @Operation(summary = "Delete user information", description = "Delete user information from the system")
   @SecurityRequirement(name = "bearerAuth")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "204", description = "Delete user information successful", content = @Content),
       @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content)})
-  public ResponseEntity<Void> delete(HttpServletRequest servlet,
-      @PathVariable String username) {
+  @Tags({@Tag(name = "Delete")})
+  public ResponseEntity<Void> delete(HttpServletRequest servlet, @PathVariable String username) {
     userService.delete(servlet, username);
     return ResponseEntity.noContent().build();
   }
